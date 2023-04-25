efba1c5ca669daa8dd92f31830a2ac1d21ae6fe5
Jen Ho <jenny.hofun@gmail.com>
2023-04-25 02:27:21 -0700

    updated script file to get to end game and save user's initial to local store.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,75 +1,90 @@
 //Create a pool of questions and answers
 var qPool = [
   {
-    question: "What is the output of the following code? \n console.log(typeof null);",
+    question:
+      "What is the output of the following code? \n console.log(typeof null);",
     choices: ["undefined", "null", "object", "number"],
-    correctAnswer: 2
+    correctAnswer: 2,
   },
   {
     question: "Which of the following is a falsy value in JavaScript?",
     choices: ["0", "''", "NaN", "All of the above"],
-    correctAnswer: 3
+    correctAnswer: 3,
   },
   {
     question: "What is the result of 2 + '2'?",
     choices: ["4", "'22'", "22", "NaN"],
-    correctAnswer: 1
+    correctAnswer: 1,
   },
   {
-    question: "Which of the following is not a primitive data type in JavaScript?",
+    question:
+      "Which of the following is not a primitive data type in JavaScript?",
     choices: ["string", "number", "boolean", "array"],
-    correctAnswer: 3
+    correctAnswer: 3,
   },
   {
-    question: "What is the difference between '==' and '===' operators in JavaScript?",
-    choices: ["Both operators check for value and type equality", "'==' checks only for value equality, while '===' checks for value and type equality", "'==' checks for value and type equality, while '===' checks only for value equality", "None of the above"],
-    correctAnswer: 1
+    question:
+      "What is the difference between '==' and '===' operators in JavaScript?",
+    choices: [
+      "Both operators check for value and type equality",
+      "'==' checks only for value equality, while '===' checks for value and type equality",
+      "'==' checks for value and type equality, while '===' checks only for value equality",
+      "None of the above",
+    ],
+    correctAnswer: 1,
   },
   {
-    question: "What is the output of the following code? \n console.log('2' + 2 - '2');",
+    question:
+      "What is the output of the following code? \n console.log('2' + 2 - '2');",
     choices: ["NaN", "02", "4", "22"],
-    correctAnswer: 2
+    correctAnswer: 2,
   },
   {
     question: "What is the result of typeof typeof 1?",
     choices: ["number", "string", "undefined", "boolean"],
-    correctAnswer: 1
+    correctAnswer: 1,
   },
   {
-    question: "What is the output of the following code? \n console.log(typeof []);",
+    question:
+      "What is the output of the following code? \n console.log(typeof []);",
     choices: ["object", "array", "undefined", "function"],
-    correctAnswer: 0
+    correctAnswer: 0,
   },
   {
-    question: "What is the result of the following code? \n console.log(1 + -'1' + '2');",
+    question:
+      "What is the result of the following code? \n console.log(1 + -'1' + '2');",
     choices: ["12", "02", "NaN", "02-1"],
-    correctAnswer: 3
+    correctAnswer: 3,
   },
   {
-    question: "What is the output of the following code? \n console.log('1' - - '1');",
+    question:
+      "What is the output of the following code? \n console.log('1' - - '1');",
     choices: ["0", "2", "'11'", "-1"],
-    correctAnswer: 1
+    correctAnswer: 1,
   },
   {
-    question: "What is the output of the following code? \n console.log(null == undefined);",
+    question:
+      "What is the output of the following code? \n console.log(null == undefined);",
     choices: ["true", "false", "undefined", "null"],
-    correctAnswer: 0
+    correctAnswer: 0,
   },
   {
-    question: "What is the output of the following code? \n console.log(+'3' + 4);",
+    question:
+      "What is the output of the following code? \n console.log(+'3' + 4);",
     choices: ["7", "34", "NaN", "undefined"],
-    correctAnswer: 0
+    correctAnswer: 0,
   },
   {
     question: "What is the result of '5' + 3 + 2?",
     choices: ["10", "'532'", "15", "'53' + 2"],
-    correctAnswer: 1
+    correctAnswer: 1,
   },
   {
-    question: "What is the output of the following code? \n console.log('hello'.charAt(0));",
+    question:
+      "What is the output of the following code? \n console.log('hello'.charAt(0));",
     choices: ["'h'", "'e'", "'l'", "'o'"],
-    correctAnswer: 0
-  }
+    correctAnswer: 0,
+  },
   //add more questions here
 ];
 
@@ -84,20 +99,22 @@ var modalBody = document.querySelector(".modalBody");
 var modalFooter = document.querySelector(".modalFooter");
 var modalEnd = document.querySelector("#modal-End");
 var finalScoreMsg = document.querySelector("#finalScoreMsg");
+var modalAlertMsg = document.querySelector(".modalAlertMsg");
 var saveBtn = document.querySelector("#saveScore");
-var initalInput = document.querySelector("#initials");
+var initialInput = document.querySelector("#initials");
 var gameQuestions = []; //empty array to hold 10 shuffled selected questions
 var timeLeft;
 var timer;
 var currentQuestionIndex = 0;
-// var finalScore;
+var savedScores = [];
+
+
 
 startButton.addEventListener("click", function () {
-    
-    modalStart.style.display = "none";
-    startGame();
+  modalStart.style.display = "none";
+  startGame();
 });
-    
+
 function startGame() {
   //create an array of 10 questions randomly from the pool of questions
   shuffleQuestions();
@@ -106,11 +123,9 @@ function startGame() {
   countdown();
   //render the first question in the gameQuestions array to the dom
   renderQuestion();
- 
 }
 
-function shuffleQuestions() 
-{
+function shuffleQuestions() {
   //pick 10 random questions from the pool of questions.
   while (gameQuestions.length <= 9) {
     var randomQuestion = qPool[Math.floor(Math.random() * qPool.length)];
@@ -144,37 +159,31 @@ function countdown() {
   }, 1000);
 }
 
-
-
-
 //function to handle displaying question in the gameQuestions array to the dom
 function renderQuestion() {
-  
   var currentQuestion = gameQuestions[currentQuestionIndex];
-  var p = document.createElement("p");;
+  var p = document.createElement("p");
   p.textContent = currentQuestion.question;
-  //clear the question from the previous 
-  questionElement.innerHTML = ""; 
+  //clear the question from the previous
+  questionElement.innerHTML = "";
   questionElement.appendChild(p);
 
   //clear the options list from the previous question
   optionsList.innerHTML = "";
-    
+
   //create and appand each options for the question as a button
   currentQuestion.choices.forEach((choice, index) => {
-  
     //create list item number [i]
     var li = document.createElement("li");
     li.setAttribute("data-index", index);
     //list item contain a button that display the question's option number [i]
     var button = document.createElement("button");
     button.textContent = choice;
-    
+
     li.appendChild(button);
     optionsList.appendChild(li);
-
   });
-};
+}
 
 //function to handle user's answer selection
 optionsList.addEventListener("click", function (event) {
@@ -191,49 +200,42 @@ function checkAnswer(selectedAnswer) {
   //get current question in array
   var currentQuestion = gameQuestions[currentQuestionIndex];
 
-  
   //if the user selected the correct answer
-  if (selectedAnswer === currentQuestion.correctAnswer)
-  {
+  if (selectedAnswer === currentQuestion.correctAnswer) {
     //then it is at the last question
-    if(currentQuestionIndex == 9)
-    {
+    if (currentQuestionIndex == 9) {
       //end the game
 
       endGame();
     }
     //else if it is not last question, edvance to next question
-    else{
+    else {
       currentQuestionIndex++;
       renderQuestion();
     }
-    
   }
   //if the user selected the wrong answer
-  else if( selectedAnswer !== currentQuestion.correctAnswer)
-  {
+  else if (selectedAnswer !== currentQuestion.correctAnswer) {
     //if it is at the last question
-    if(currentQuestionIndex == 9)
-    {
+    if (currentQuestionIndex == 9) {
       //timer take 5 sec off and game over
       //endGame will zero out all negative timeLeft amount as the final result
       timeLeft = timeLeft - 5;
       endGame();
-    }
-    else{
+    } else {
       //else if it is not last question, timer - 5 and advance to next question
       timeLeft = timeLeft - 5;
       currentQuestionIndex++;
       renderQuestion();
-    } 
+    }
   } //just in case there are any other cases
-  else{
-    endGame()
+  else {
+    endGame();
   }
 }
 
-// GAME OVER 
-function endGame(){
+// GAME OVER
+function endGame() {
   var userName = "";
   var userEmail = "";
   var finalScore = timeLeft;
@@ -250,40 +252,58 @@ function endGame(){
 
   //show the modal
   modalEnd.style.display = "block";
-  
-  //show final score to modal body
-  finalScoreMsg.innerText = "Your final score is: " + timeLeft;
 
+  //show final score to modal body
+  finalScoreMsg.innerText = "Your final score is: " + finalScore;
 
   //add event listener to submit button
   saveBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    //get the value of the text input to the inital field
    
-  //get the value of the text input to the inital field
-  var initials = initalInput.value;
+    while(initialInput.value === "") 
+    {
+      alert("Please enter your initials");
+      return;
+    } 
+    var save = 
+    {
+      initials: initialInput.value.trim(),
+      score: finalScore,
+    };
 
-  //store it to local storage
-  storeScore(initials, timeLeft);
-
-  //clear the modal and output score saved after submit
-  modalEnd.style.display = "none";
-   
+      // Add new save to savedScores array, clear the input
+      savedScores.push(save);
+      initialInput.value = "";
+      localStorage.setItem("savedScores", JSON.stringify(savedScores));
+      alert("Success!!! Your score has been saved!");
+      window.location.reload();
+      
   });
 
+  // renderSavedScore();
 }
 
-function storeScore(initials,finalScore) {
-  
-  // Get stored scores from localStorage, or if not any, set to empty array
-  var storedScores = JSON.parse(localStorage.getItem("scores")) || [];
-  // Push new score into scores array
-  storedScores.push(userResult);
-} 
+// function renderSavedScore() {
+// modalEnd.style.display = "none";
+// modalStart.style.display = "none";
+// questionElement.innerHTML = "";
 
+//   // Render a new li for each todo
+//   for (var i = 0; i < savedScores.length; i++) {
+//     var save = savedScores[i];
 
+//     var li = document.createElement("li");
+//     li.textContent = "In
+//     li.setAttribute("data-index", i);
 
+//     var button = document.createElement("button");
+//     button.textContent = "Complete ✔️";
 
+//     li.appendChild(button);
+//     todoList.appendChild(li);
+//   }
 
+// var highScores = JSON.parse(localStorage.getItem("saves"));
 
-
-
-
+// if(lo)
